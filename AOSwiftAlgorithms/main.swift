fcaf7e601d8ad4a5ec9c09f845e6a318650b57fb
Altynbek Orumbayev <stay.in.there@gmail.com>
2017-09-18 16:47:36 +0600

    Created xcodeproj

---
+++ AOSwiftAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AOSwiftAlgorithms
+//
+//  Created by Altynbek Orumbayev on 9/18/17.
+//  Copyright © 2017 Altynbek Orumbayev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
