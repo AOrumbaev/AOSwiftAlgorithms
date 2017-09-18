9754ac28fa1706c3e67e1ee55350b14ee29e4f06
Altynbek Orumbayev <stay.in.there@gmail.com>
2017-09-18 17:47:31 +0600

    Added 'Queue' implementations

---
+++ AOSwiftAlgorithms/Data Structures/Queue.swift
@@ -0,0 +1,73 @@
+
+//
+//  Queue.swift
+//  AOSwiftAlgorithms
+//
+//  Created by Altynbek Orumbayev on 9/18/17.
+//  Copyright © 2017 Altynbek Orumbayev. All rights reserved.
+//
+
+import Foundation
+
+struct Queue<T> {
+    
+    fileprivate var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return isEmpty ? nil : array.removeFirst()
+    }
+    
+    public var front: T? {
+        return array.first
+    }
+}
+
+struct EfficientQueue<T> {
+    
+     var array = [T?]()
+     var head = 0
+    
+    public var isEmpty: Bool {
+        return count == 0
+    }
+    
+    public var count: Int {
+        return array.count - head
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        guard head < array.count, let element = array[head] else {return nil}
+        
+        array[head] = nil
+        head += 1
+        
+        let percentage = Double(head)/Double(array.count)
+        if array.count > 50 && percentage > 0.25 {
+            array.removeFirst()
+            head = 0
+        }
+        
+        return element
+    }
+    
+    public var front: T? {
+        return isEmpty ? nil : array[head]
+    }
+}
+
--- AOSwiftAlgorithms/main.swift
+++ AOSwiftAlgorithms/main.swift
@@ -14,5 +14,18 @@ var testStack = Stack<String>()
 testStack.push("hello")
 testStack.push("world")
 print(testStack)
-testStack.pop()
-print(testStack)
+let lastItem = testStack.pop()
+print("\(testStack) last item \(lastItem)")
+
+//MARK: - Queue
+var testQueue = Queue<Int>()
+testQueue.enqueue(4)
+testQueue.enqueue(2)
+testQueue.enqueue(19)
+print(testQueue)
+let firstItem = testQueue.dequeue()
+print("\(testQueue) first item \(firstItem)")
+
+//MARK: - EfficientQueue
+var q = EfficientQueue<String>()
+print(q.array)
